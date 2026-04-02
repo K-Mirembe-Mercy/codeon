c38ae9439181ff8e49eb86d0e6cb784ea6caf89f
K-Mirembe-Mercy <133584067+K-Mirembe-Mercy@users.noreply.github.com>
2026-04-02 07:52:51 +0300

    Add PythonCertificate component for user certificates
    Signed-off-by: K-Mirembe-Mercy <133584067+K-Mirembe-Mercy@users.noreply.github.com>

---
+++ Courses/src/components/PythonCertificate.tsx
@@ -0,0 +1,7 @@
+export const PythonCertificate: React.FC<CertificateProps> = ({ userName, completionDate, verificationCode }) => {
+  return (
+    <div className="certificate-container">
+      {/* Ornate design with "Software Engineering & Python Programming" branding */}
+    </div>
+  );
+};
